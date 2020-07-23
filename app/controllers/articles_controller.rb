eadfecf53b9852f9491768f2e6701172fb96ca1a
jordiebrad <jordiebrad@gmail.com>
2020-07-23 11:25:25 -0700

    started new and create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+
   def index
     @articles = Article.all
   end
+
+  def new
+    
+  end
+
+  def create
+    render plain: params[:article]
+  end
+  
+  
+  
 end
